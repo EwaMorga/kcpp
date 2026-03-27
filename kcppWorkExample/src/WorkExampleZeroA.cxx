19a78f14455e3be579dbde35f9e258f968701198
EwaMorga <emorga@us.edu.pl>
2026-03-27 14:13:11 +0100

    my second commit

---
+++ kcppWorkExample/src/WorkExampleZeroA.cxx
@@ -0,0 +1,16 @@
+/* Author: Ewa Morga
+   date: 2026.03.20, v0.01
+ */
+
+#include "WorkExampleZero.h"
+#include <iostream>
+using namespace std;
+
+
+
+void WorkExampleZero::WorkExampleA() {
+
+  cout << "---------------------------------------" << endl;
+  cout << "Wartość WorkExampleA:" << x << endl;
+  
+}
